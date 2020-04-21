bcaa291f4a4ed6a6858c9ec5fe6ae251954688bb
SirPenGoo <59529037+SirPenGoo@users.noreply.github.com>
2020-04-21 09:24:12 +0200

    push template

---
+++ SList/SList/main.c
@@ -0,0 +1,17 @@
+/*==========================================================
+|	Assignment:		SWE02_HUE04
+|	File:			SList.c
+|	Author:			Maximilian Erlmoser
+|	University:		FH Salzburg
+|	Semester:		ITS-B SS20
+|	Date:			21.04.2020
+|-----------------------------------------
+|	Description:	Double linked list
+*=========================================================*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main() {
+
+}
